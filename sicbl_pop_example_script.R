689a3bc1de9f6aad21f56d95e12ccd4ce86de687
Grace Libby <grace.libby@nhs.net>
2024-08-16 12:28:45 +0100

    update example script with NHS region code

--- sicbl_pop_example_script.R
+++ sicbl_pop_example_script.R
@@ -1,6 +1,11 @@
-#example script using functions from NHS dental statistics repo
-#to get population by sub-ICB in England from 2019 to 2022 (latest available)
-#excludes Isles of Scilly
+# example script using functions from NHS dental statistics repo
+# to get population by sub-ICB in England from 2019 to 2022 (latest available)
+# excludes Isles of Scilly
+
+#library(dplyr)
+#library(openxlsx)
+#library(readr)
+#library(tidyr)
 
 # source functions
 # this is only a temporary step until all functions are built into packages
@@ -30,7 +35,7 @@ lsoa_df_2020 <- lsoa_list$lsoa_2020
 lsoa_df_2021 <- lsoa_list$lsoa_2021
 lsoa_df_2022 <- lsoa_list$lsoa_2022
 
-#get population data for sub-icb by year
+# get population data for sub-icb by year
 sub_icb_pop <- get_eng_sicb_pop(
   lsoa11_lookup = lsoa11_lookup,
   lsoa21_lookup = lsoa21_lookup,
@@ -40,11 +45,36 @@ sub_icb_pop <- get_eng_sicb_pop(
   lsoa_df_2022 = lsoa_df_2022
 )
 
-#checking unique values
-#using 'CD' columns gives 2 extra
-length(unique(sub_icb_pop[["SICBLCD"]]))
-length(unique(sub_icb_pop[["ICBCD"]]))
+# checking if join and summarise steps give expected number of unique values
 
-#using CDH columns gives correct numbers (42 unique ICB codes, 106 unique SICBL codes)
-length(unique(sub_icb_pop[["ICBCDH"]]))
-length(unique(sub_icb_pop[["SICBLCDH"]]))
+# using 'CD' columns gives 2 extra
+
+#length(unique(sub_icb_pop[["SICBLCD"]]))
+#length(unique(sub_icb_pop[["ICBCD"]]))
+
+# using CDH columns gives correct numbers (42 unique ICB codes, 106 unique SICBL codes)
+
+#length(unique(sub_icb_pop[["ICBCDH"]]))
+#length(unique(sub_icb_pop[["SICBLCDH"]]))
+
+# population by region
+# to be put into a function in future
+region_22_lookup <- lookups_list$region_22
+region_23_lookup <- lookups_list$region_23
+
+# create table mapped and aggregated up to population in NHS England Regions
+nhs_eng_region_pop <- sub_icb_pop |>
+  #fix code for Surrey Heartlands SICBL and Sussex SICBL to newest ONS SICBL code
+  dplyr::mutate(SICBLCD = dplyr::case_when(
+    SICBLCD == "E38000248" ~ "E38000265",
+    SICBLCD == "E38000246" ~ "E38000264",
+    TRUE ~ SICBLCD
+  )) |>
+  #join lookup to NHS region
+  dplyr::left_join(region_23_lookup, by = c("SICBLCD" = "SICBL23CD")) |>
+  dplyr::group_by(year, NHSER23CD, NHSER23CDH, NHSER23NM) |>
+  dplyr::summarise(region_population = sum(sicbl_population)) |>
+  ungroup()
+
+# check if 7 NHS regions present in final data  
+#length(unique(sicb_region_pop[["NHSER23CD"]]))
